2ca9318cd6b3da067e84c45f6eee051af354c057
Eldunari <61095035+Eldunarii@users.noreply.github.com>
2023-02-15 15:51:02 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,15 +1,9 @@
-CREATE DATABASE IF NOT EXISTS intersdb;
+CREATE DATABASE mydatabase;
+USE mydatabase;
 
-USE intersdb;
-
-CREATE TABLE intervenants (
-   id INT PRIMARY KEY,
-   nom VARCHAR(50) NOT NULL,
-   prenom VARCHAR(50) NOT NULL,
-   email VARCHAR(100) UNIQUE,
-   telephone VARCHAR(20),
-   adresse VARCHAR(200),
-   ville VARCHAR(50),
-   code_postal VARCHAR(10),
-   pays VARCHAR(50)
+CREATE TABLE inters (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
 );
